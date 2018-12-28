03c60076b588c2cccaa63941000a6d66153fd59d
Admin <raunaksingh@toshiba.home>
2018-12-27 22:01:12 -0500

    I added a new file

--- python_for_tweens_and_teens/chapter_11/random_word_three_letters.py
+++ python_for_tweens_and_teens/chapter_11/random_word_three_letters.py
@@ -1,2 +1,10 @@
+import random as ran
 
+abc = "abcdefghijklmnopqrstuvwxyzdsw"
 
+letter1 = abc[ran.randrange(len(abc))]
+letter2 = abc[ran.randrange(len(abc))]
+letter3 = abc[ran.randrange(len(abc))]
+
+word = letter1 + letter2 + letter3
+print(word)
