04fc2ae2a78bb3bd85cab81310301023f65cf34d
Admin <raunaksingh@toshiba.home>
2018-12-28 21:25:08 -0500

    Changed files in chapter 13

---
+++ python_for_tweens_and_teens/chapter_13/find_middle_age.py
@@ -0,0 +1,10 @@
+first_age = input("first age ")
+second_age = input("second age ")
+third_age = input("third age ")
+
+list = [first_age, second_age, third_age]
+
+
+        
+print("the list from greatest ot least is,", list)
+print("the middle age is", list[1])
