25f19d19601800b451c3cf7f16e5bcd77147b198
Raunak Singh <Jasmeen_walia@yahoo.com>
2018-12-31 22:18:15 -0500

    created new file excersize_19.1.2.py in /Documents/coding/learning_python/python_for_tweens_and_teens/chapter_19/

---
+++ python_for_tweens_and_teens/chapter_19/excersize_19.1.2.py
@@ -0,0 +1,10 @@
+# Find out what is displayed
+# x input value is 3
+x = int(input("the value of x")) 
+
+# range creates sequence:
+# 6, 5, 4, 3
+for i in range(6, x - 1, -1): 
+    print(i)
+
+#6, 5, 4, 3 are displayed
