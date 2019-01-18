35b79832a6c21ca66c1c3f05e809d25f05b54a15
Raunakoo <jasmeen_walia@yahoo.com>
2019-01-18 13:26:35 -0500

    finalized last I worked

---
+++ python_for_tweens_and_teens/chapter_20/exc_3.py
@@ -0,0 +1,13 @@
+for i in range(5,0,-1):
+    for j in range(5,1,-1):
+        for x in range(5,2,-1):
+            for y 
+
+            
+            if i == j and j == x:
+                print(i,j,x)
+        if i == j and j == 2:
+            print(i, j)
+    if i == 1:
+        print(i)
+        
